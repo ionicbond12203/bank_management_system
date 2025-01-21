a5ac8909539b1fc446c6575d6ef7494302518660
Ir0n <129019822+JY1104@users.noreply.github.com>
2025-01-22 00:04:07 +0800

    Update account.h

--- account.h
+++ account.h
@@ -7,13 +7,14 @@ using namespace std;
 
 class Account {
 public:
+    // Public member functions
     void check_account(const string& user_name);
     void transfer(const string& sender_name);
     void update_balance(const string& user_name, double new_balance);
     void add_transaction(const string& user_name, const string& transaction_details);
     void top_up(const string& user_name);
-    void view_transaction_history(const string& user_name);
-    void delete_account(const string& user_name);
+    void view_transaction_history(const string& user_name);  // Function declaration
+    void delete_account(const string& user_name);  // Function declaration
 
 private:
     // Structure to store transaction details
@@ -25,10 +26,16 @@ private:
         string sender;
     };
 
-    // Private helper functions for transaction sorting
-    Transaction parseTransactionLine(const string& line);
+    // Private helper functions for transactions
     vector<Transaction> readTransactions(const string& filePath);
     void sortAndDisplayTransactions(const string& user_name, const string& criteria, const string& order);
+
+    // QuickSort and partitioning functions
+    void quickSort(vector<Transaction>& transactions, int low, int high, const string& criteria, const string& order);
+    int partition(vector<Transaction>& transactions, int low, int high, const string& criteria, const string& order);
+
+    // Helper function to parse each transaction line
+    Transaction parseTransactionLine(const string& line);
 };
 
 #endif // ACCOUNT_H

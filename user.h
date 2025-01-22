c7d084c661b8c81a610946f60797d456e79458cb
Ir0n <129019822+JY1104@users.noreply.github.com>
2025-01-22 10:04:09 +0800

    Update user.h

--- user.h
+++ user.h
@@ -8,8 +8,6 @@ class User {
 public:
     void userInfoFillIn(const string& user_name);
     void createUserFile(const string& user_name, const int& age, const string& Fname, const string& Lname, const string& IC, const string& birthday, const string& email, const string& address);
-    void view_user_info(string& user_name);
-    void editUserInfo(const string& user_name);
     void createBalanceFile(const string& user_name);
 };
 

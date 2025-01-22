a6c4934ae8bcedf8d5cb67db5afc73fdcf8b0d67
Ir0n <129019822+JY1104@users.noreply.github.com>
2025-01-22 08:35:58 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "account.h"
 #include "settings.h"
 #include <windows.h>
+#include <conio.h>
 
 
 using namespace std;
@@ -27,26 +28,29 @@ void preHome(string user_name, string password, string balance) {
     int choice;
     do {
         cout << "Welcome to the Bank System\n";
-        cout << "1. Login\n2. Register\n3. Exit\nEnter your choice: ";
+        cout << "1. Login🔑\n2. Register✍️\n3. Exit❌\nEnter your choice: ";
         cin >> choice;
 
         if (choice == 1) {
-            cout << "Enter username: ";
+            cout << "Enter username👤: ";
             cin >> user_name;
-            cout << "Enter password: ";
+            cout << "Enter password🔑: ";
             cin >> password;
 
             if (auth.isAccountLocked(user_name)) {
-                cout << "Your account is locked. Please try later.\n";
+                cout << "\033[31mYour account is locked🔒. Please try later.\033[37m\n";
+                system("pause");
                 return;
             }
 
             if (auth.signIn(user_name, password)) {
-                cout << "Login successful!\n";
+                cout << "\033[32m✅Login successful!✅\033[37m\n";
+                system("pause");
                 auth.resetFailedAttempts(user_name);
                 displayHomeScreen(user_name, balance, password);
             } else {
-                cout << "Invalid username or password.\n";
+                cout << "\033[31m\n❌Invalid username or password.❌\033[37m\n";
+                system("pause");
                 auth.recordFailedAttempt(user_name);
             }
         } else if (choice == 2) {
@@ -54,7 +58,8 @@ void preHome(string user_name, string password, string balance) {
             cin >> user_name;
 
             if (auth.usernameExists(user_name)) {
-                cout << "Username already exists. Please choose another.\n";
+                cout << "\033[33m⚠️Username already exists. Please choose another.⚠️\033[37m\n";
+                system("pause");
                 continue;
             }
 
@@ -62,17 +67,21 @@ void preHome(string user_name, string password, string balance) {
             cin >> password;
 
             if (auth.registerUser(user_name, password)) {
-                cout << "Registration successful!\n";
+                cout << "\033[32mRegistration successful!\033[37m\n";
+                system("pause");
                 user.userInfoFillIn(user_name);
                 user.createBalanceFile(user_name);
             } else {
-                cout << "Registration failed.\n";
+                cout << "\033[31m❌Registration failed.❌\033[37m\n";
+                system("pause");
             }
         } else if (choice == 3) {
-            cout << "Exiting the application.\n";
+            cout << "\033[31mExiting the application.\033[37m\n";
+            system("pause");
             break;
         } else {
-            cout << "Invalid choice. Please try again.\n";
+            cout << "\033[31m❌Invalid choice. Please try again.❌\033[37m\n";
+            system("pause");
         }
     } while (choice != 3);
 }
@@ -85,7 +94,7 @@ void displayHomeScreen(string user_name, string balance, string password) {
     do {
         system("CLS");
         cout << "Welcome to the Home Screen, " << "\033[32m" << user_name << "\033[0m!" << endl;
-        cout << "1. 🎯Check Account\n2. 🔁Transfer\n3. 💳Top Up\n4. 📜View Transaction History\n5. ⚙️Settings\n6. 🗑️Delete Account🗑\n7. 🔚Logout" << endl;
+        cout << "1. 🎯Check Account\n2. 🔁Transfer\n3. 💳Top Up\n4. 📜View Transaction History\n5. ⚙️Settings\n6. 🗑️Delete Account️\n7. 🔚Logout" << endl;
         cout << "Select an option: ";
         cin >> option;
 
@@ -107,6 +116,7 @@ void displayHomeScreen(string user_name, string balance, string password) {
                 break;
             case 6:
                 account.delete_account(user_name);
+                break;
             case 7:
                 system("CLS");
                 cout << "Logging out." << endl;
@@ -117,6 +127,7 @@ void displayHomeScreen(string user_name, string balance, string password) {
                 system("CLS");
                 cout << "Logging out..." << endl;
                 system("pause");
+                system("cls");
                 return; // Ends the home screen loop to return to preHome
             default:
                 cout << "\033[31m❌Invalid Input, try again❌\033[0m\n" << endl;
